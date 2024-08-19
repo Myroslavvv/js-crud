0d5fda5e98f3344200cf522db892a33b5d2100d5
Myroslavvv <pticasw@gmail.com>
2024-08-19 16:01:14 +0300

    js-crud-product4

--- src/route/index.js
+++ src/route/index.js
@@ -10,13 +10,10 @@ class Product {
 
   constructor(name, price, description) {
     this.id = Math.floor(Math.random() * (99999 - 10000 + 1)) + 10000;
-    // this.id = new Date().getTime();
     this.name = name;
     this.price = price;
     this.description = description;
   }
-
-  // verifyPassword = (password) => this.password === password
 
   //Це статичний метод add, який приймає параметр product і додає його до масиву #list за допомогою методу push.
   static add = (product) => {
@@ -28,27 +25,13 @@ class Product {
   static getById = (id) =>
   this.#list.find((product) => product.id === id);
   
-  // static deleteById = (id) => {
-  //   const product = this.getById(id)
-  //   if (product) {
-  //     if (id) {
-  //       product.id = id
-  //     }
-  //     return true
-  //   } else {
-  //     return false
-  //   }
-  // };
-
-
- 
   static deleteById = (id) => {
     const index = this.#list.findIndex((product) => product.id === id);
     if (index !== -1) {
-      this.#list.splice(index, 1); // Удаляем продукт из массива
-      return true; // Возвращаем true, если продукт был удален
+      this.#list.splice(index, 1); // Видаляєм продукт із масива
+      return true; // повертаєм true, якщо продукт видалено
     }
-    return false; // Возвращаем false, если продукт не найден
+    return false; // повертаєм false, якщо продукт не знайдено
   };
 
   
@@ -63,47 +46,13 @@ class Product {
           product.price = price
           product.description = description
         }
-        // this.update(product, {name})
         return true
       } else {
         return false
       }
     }
     
-    // static update = (user, {email}) => {
-      //   if(email) {
-        //     user.email = email
-        //   }
-        
-        
-      }
-// ================================================================
-
-// router.get('/product-create', function (req, res) {
-//   const{id} = req.query
-//   User.deleteById(Number(id))
-//   res.render('success-info', {
-//     style: 'success-info',
-//     info: "Користувач створений",
-//   })
-// })
-
-// ================================================================
-
-// router.post('/product-create', function (req, res) {
-//   const{createDate, name, price, description} = req.body
-//   let result = false;
-//   const user = Product.getById(Number(id))
-//   if(product.verifyPassword(password)) {
-//     Product.update(product, {email})
-//     result = true;
-//   }
-
-//   res.render('success-info', {
-//     style: 'success-info',
-//     info: result ? "Емайл пошта оновлена" : "Сталася помилка",
-//   })
-// })
+}
 
 // ================================================================
 
@@ -131,9 +80,6 @@ router.get('/', function (req, res) {
 })
 
 // ================================================================
-
-    // console.log(Product.getList()); // Выводит массив продуктов/
-    // console.log(`Product ID: ${productId}`); // Проверка ID продукта
 
 router.get('/product-create', (req, res) => {
   res.render('product-create')
@@ -166,19 +112,6 @@ router.get('/product-list', (req, res) => {
     },
   })
 })
-
-// router.post('/product-list', function (req, res) {
-//   const list = Product.getList()
-//   res.render('product-list', {
-//     style: 'product-list',
-//     data: {
-//       products: {
-//         list,
-//         isEmpty: list.length === 0,
-//       },
-//     },
-//   })
-// })
 
 // ================================================================
 
@@ -215,124 +148,7 @@ router.post('/product-edit', (req, res) => {
   }
 });
 
-// router.post('/product-edit', (req, res) => {
-//   const id = Number(req.body.id)
-//   const { name, price, description } = req.body
-//   if (!price) {
-//   if (!name || !price || !description) {
-//   const product = Product.getById(id)
-//   if (product) {
-//     Product.updateById(id, { name, price, description })
-//     res.render('alert', {
-//       style: 'alert',
-//       info6: 'Продукт оновлено успішно',
-//     })
-//   } else {
-//     res.status(404).send('Продукт не знайдено')
-//   }
-//   } else {
-//   res.status(404).send('Нема ціни продукта');
-//   }
-// })
-
-// router.post('/product-edit', (req, res) => {
-//   try {
-//     const { id, name, price, description } = req.body;
-
-//     if (!id || !name || !price || !description) {
-//       return res.status(400).send('Все поля являются обязательными');
-//     }
-
-//     // Проверка, является ли цена числом
-//     if (isNaN(price)) {
-//       return res.status(400).send('Цена должна быть числом');
-//     }
-
-//     const product = Product.getById(Number(id));
-//     if (product) {
-//       Product.updateById(id, { name, price, description });
-//       res.render('alert', {
-//         style: 'alert',
-//         info6: 'Продукт обновлен успешно',
-//       });
-//     } else {
-//       res.status(404).send('Продукт не найден');
-//     }
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).send('Произошла ошибка на сервере');
-//   }
-// });
-
-// console.log(name, price, id, description);
-
-// router.get('/product-edit', (req, res) => {
-// const {id} = req.query;        //отримати з req.query властивість id
-// Product.getById(Number(id));    //
-// const id = req.query.id;
-// const product = new Product(price, description);
-// Product.add(product);
-// console.log(Product.getList());
-// res.render('product-edit', {
-// style: '/product-edit',
-// info3: "Товар з таким ID не знайдено",
-// info3: result ? "Дані редаговано" : "Сталася помилка",
-// });
-// });
-
-// router.post('/product-edit', function (req, res) {
-//   const {name, price, description} = req.body;
-//   const product = new Product(name, price, description);
-//   Product.add(product);
-//   console.log(Product.getList());
-//   res.render('alert', {
-//     style: 'alert',
-//     info1: "Успішне виконання дії",
-//     info2: "Товар створений"
-//   })
-// })
-
-// router.post('/product-edit', function (req, res) {
-//   const {name, price, id, description} = req.body;
-//   console.log(name, price, id, description);
-
-//   const result = Product.updateById(Number(id), {name}, {price}, {description})
-//   res.render('alert', {
-//     style: 'alert',
-//     // info1: "Успішне виконання дії",
-//     info4: result ? "Дані редаговано" : "Сталася помилка",
-//   });
-// });
-
 // ================================================================
-// router.get('/product-delete', function (req, res) {
-//   const { id } = req.query;
-//   Product.deleteById(Number(id)); 
-  
-//   res.render('alert', {
-//     style: 'alert',
-//     info1: 'Успішне виконання дії',
-//     info4: 'Товар видалений',
-//   })
-// });
-
-// router.get('/product-delete', function (req, res) {
-//   const { id } = req.query;
-//   const success = Product.deleteById(Number(id)); // Проверка успешности удаления
-//   if (success) {
-//     res.render('alert', {
-//       style: 'alert',
-//       info1: 'Успішне виконання дії',
-//       info4: 'Товар видалений',
-//     });
-//   } else {
-//     res.render('alert', {
-//       style: 'alert',
-//       info1: 'Помилка',
-//       info4: 'Товар не знайдено',
-//     });
-//   }
-// });
 
 router.get('/product-delete', function (req, res) {
   const { id } = req.query;
@@ -353,46 +169,7 @@ router.get('/product-delete', function (req, res) {
   }
 });
 
-
-// router.get('/product-delete', async function (req, res) {
-//   const { id } = req.query;
-//   try {
-//     await Product.deleteById(Number(id));
-//     res.render('alert', {
-//       style: 'alert',
-//       info1: 'Успішне виконання дії',
-//       info4: 'Товар видалений',
-//     });
-//   } catch (error) {
-//     res.render('alert', {
-//       style: 'alert',
-//       info1: 'Помилка',
-//       info4: 'Не вдалося видалити товар',
-//     });
-//   }
-// });
 // ================================================================
-// router.get('/user-delete', function (req, res) {
-//   const{id} = req.query                     //req.query-при get отримання даних з строки запроса браузера(часть URL после символа ?(напр.?id=123)
-//   User.deleteById(Number(id))                 //(deleteById - знаходяться в класі class User)
-//   res.render('success-info', {
-//     style: 'success-info',
-//     info: "Користувач видалений",
-//   })
-// })
 
-// router.post('/product-delete', function (req, res) {
-//   const {name, price, id, description} = req.body;
-//   console.log(name, price, id, description);
-
-//   const result = Product.deleteById(Number(id), {name}, {price}, {description})
-//   res.render('alert', {
-//     style: 'alert',
-//     // info1: "Дані редаговано",
-//     info5: result ? "Товар виделено" : "Сталася помилка",
-//   });
-// });
-
-// ================================================================
 // Підключаємо роутер до бек-енду
 module.exports = router
